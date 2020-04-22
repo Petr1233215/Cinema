493f4542b08f5d2f1f16eed2246e1143d3ce6dbd
Petr1233215 <zero0one1two4@gmail.com>
2020-04-23 00:54:24 +0500

    Добавил создание объекта модели в контроллере и передача ему параметров. Также добавил отрисовку View

--- Cinema/Controllers/AccountController.cs
+++ Cinema/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Cinema.Models;
 
 namespace Cinema.Controllers
 {
@@ -11,7 +12,8 @@ namespace Cinema.Controllers
         // GET: Account
         public ActionResult Login()
         {
-            return View();
+			var model = new LoginModel(){Login="Piter", Password = "Piter"};
+            return View(model);
         }
     }
 }
