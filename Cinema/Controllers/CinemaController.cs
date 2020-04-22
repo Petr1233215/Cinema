b4ae9b8977878a86a18fa39cf145400fe65df36b
Petr1233215 <zero0one1two4@gmail.com>
2020-04-22 19:53:03 +0500

    Add project files.

---
+++ Cinema/Controllers/CinemaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cinema.Models;
+
+namespace Cinema.Controllers
+{
+    public class CinemaController : Controller
+    {
+        // GET: Cinema
+        public ActionResult Index(string param)
+        {
+			ViewBag.Standart = "start ViewBag";
+            return View(new CinemaViewModel() { Author = "I'm the author the site"});
+			
+        }
+    }
+}
---
+++ Cinema/Models/CinemaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Models
+{
+	public class CinemaViewModel
+	{
+		public string Author { get; set; }
+	}
+}
