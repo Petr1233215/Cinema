e3b89539cdb151a9f6b3eea30d4d49fb531e5ad1
Petr1233215 <zero0one1two4@gmail.com>
2020-05-18 22:47:48 +0500

    Добавление модели для кинотеатра

---
+++ Cinema/Models/Domain/TimeSlot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Models.Domain
+{
+	public class TimeSlot
+	{
+		public int Id { get; set; }
+		public DateTime StartTime { get; set; }
+		public decimal Cost { get; set; }
+		public Hall Hall { get; set; }
+		public Movie Movie { get; set; }
+	}
+}
